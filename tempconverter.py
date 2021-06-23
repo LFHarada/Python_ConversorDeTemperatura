4d49fb57d853cde677b807145136fd4092fa7262
LFHarada <77625992+LFHarada@users.noreply.github.com>
2021-06-22 22:55:33 -0300

    Update tempconverter.py

--- tempconverter.py
+++ tempconverter.py
@@ -14,70 +14,75 @@ while True:
 
     print("\nDigite apenas o número correspondente.\n")
 
-    option1 = int(input("Medida de temperatura de origem: "))
+    try:
 
+        option1 = int(input("Medida de temperatura de origem: "))
 
-    if option1 == 1:  # Kelvin 
+        if option1 == 1:  # Kelvin
 
-        option2 = int(input("Medida de temperatura de destino: ")) 
+            option2 = int(input("Medida de temperatura de destino: "))
 
-        if option2 == 1:  # to Kelvin
-            kelvin = float(input('Temperatura em K: '))
-            print("\n{:.2f} K equivalem a {:.2f} K".format(kelvin, kelvin))
+            if option2 == 1:  # to Kelvin
+                kelvin = float(input('Temperatura em K: '))
+                print("\n{:.2f} K equivalem a {:.2f} K".format(kelvin, kelvin))
 
-        elif option2 == 2:  # to Fahrenheit
-            kelvin = float(input('Temperatura em K: '))
-            temp = (((kelvin - 273)*1.8) + 32)
-            print("\n{:.2f} K equivalem a {:.2f} °F".format(kelvin, temp))
+            elif option2 == 2:  # to Fahrenheit
+                kelvin = float(input('Temperatura em K: '))
+                temp = (((kelvin - 273.15)*1.8) + 32)
+                print("\n{:.2f} K equivalem a {:.2f} °F".format(kelvin, temp))
 
-        elif option2 == 3: # to Celsius
-            kelvin = float(input('Temperatura em K: '))
-            temp = kelvin - 273
-            print("\n{:.2f} K equivalem a {:.2f} °C".format(kelvin, temp))
+            elif option2 == 3: # to Celsius
+                kelvin = float(input('Temperatura em K: '))
+                temp = kelvin - 273.15
+                print("\n{:.2f} K equivalem a {:.2f} °C".format(kelvin, temp))
+
+            else:
+                print("Error")
+
+        elif option1 == 2: # Fahrenheit
+
+            option2 = int(input("Medida de temperatura de destino: "))
+
+            if option2 == 1:  # to Kelvin
+                fahrenheit = float(input('Temperatura em °F: '))
+                temp = (((fahrenheit - 32) * (5 / 9)) + 273.15)
+                print("\n{:.2f} °F equivalem a {:.2f} K".format(fahrenheit, temp))
+
+            elif option2 == 2:  # to Fahrenheit
+                fahrenheit = float(input('Temperatura em °F: '))
+                print("\n{:.2f} °F equivalem a {:.2f} °F".format(fahrenheit, fahrenheit))
+
+            elif option2 == 3: # to Celsius
+                fahrenheit = float(input('Temperatura em °F: '))
+                temp = (fahrenheit - 32) / 1.8
+                print("\n{:.2f} °F equivalem a {:.2f} °C".format(fahrenheit, temp))
+
+            else:
+                print("Error")
+
+        elif option1 == 3:  # Celsius
+
+            option2 = int(input("Medida de temperatura de destino: "))
+
+            if option2 == 1:  # to Kelvin
+                celsius = float(input('Temperatura em °C: '))
+                temp = celsius + 273.15
+                print("\n{:.2f} °C equivalem a {:.2f} K".format(celsius, temp))
+
+            elif option2 == 2:  # to Fahrenheit
+                celsius = float(input('Temperatura em °C: '))
+                temp = celsius * 1.8 + 32
+                print("\n{:.2f} °C equivalem a {:.2f} °F".format(celsius, temp))
+
+            elif option2 == 3: # to Celsius
+                celsius = float(input('Temperatura em °C: '))
+                print("\n{:.2f} °C equivalem a {:.2f} °C".format(celsius, celsius))
+
+            else:
+                print("Error")
 
         else:
-            print("Error")
+            print('\nErro, por favor, insira apenas o que se pede.')
 
-
-    elif option1 == 2: # Fahrenheit
-
-        option2 = int(input("Medida de temperatura de destino: "))
-
-        if option2 == 1:  # to Kelvin
-            fahrenheit = float(input('Temperatura em °F: '))
-            temp = (((fahrenheit - 32) * (5 / 9)) + 273)
-            print("\n{:.2f} °F equivalem a {:.2f} K".format(fahrenheit, temp))
-
-        elif option2 == 2:  # to Fahrenheit
-            fahrenheit = float(input('Temperatura em °F: '))
-            print("\n{:.2f} °F equivalem a {:.2f} °F".format(fahrenheit, fahrenheit))
-
-        elif option2 == 3: # to Celsius
-            fahrenheit = float(input('Temperatura em °F: '))
-            temp = (fahrenheit - 32) / 1.8
-            print("\n{:.2f} °F equivalem a {:.2f} °C".format(fahrenheit, temp))
-
-        else:
-            print("Error")
-
-
-    elif option1 == 3:  # Celsius
-
-        option2 = int(input("Medida de temperatura de destino: "))  
-
-        if option2 == 1:  # to Kelvin
-            celsius = float(input('Temperatura em °C: '))
-            temp = celsius + 273
-            print("\n{:.2f} °C equivalem a {:.2f} K".format(celsius, temp))
-
-        elif option2 == 2:  # to Fahrenheit
-            celsius = float(input('Temperatura em °C: '))
-            temp = celsius * 1.8 + 32
-            print("\n{:.2f} °C equivalem a {:.2f} °F".format(celsius, temp))
-
-        elif option2 == 3: # to Celsius
-            celsius = float(input('Temperatura em °C: '))
-            print("\n{:.2f} °C equivalem a {:.2f} °C".format(celsius, celsius))
-            
-        else:
-            print("Error")
+    except:
+        print('\nErro, por favor, insira apenas o que se pede.')
